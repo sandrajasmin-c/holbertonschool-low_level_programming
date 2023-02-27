50caa9df0b7f9750eb6e97dc597736ca2151c69a
sandra <6419@holbertonstudents.com>
2023-02-26 18:17:25 -0800

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,16 @@
-#!/bin/bash
-("\"Programming is like building a multilingual puzzle")
-0
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+** Return: Always 0 (Success)
+*/
+int main(void)
+{
+    
+
+
+  puts("\"Programming is like building a multilingual puzzle")
+  return (0);
+}
