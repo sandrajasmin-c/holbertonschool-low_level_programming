1384ddbc2827703557d869af76fb350ef910b7e8
Sandra <sandrajasminc@gmail.com>
2023-06-21 11:42:08 -0700

    betty style

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+
+ * main - imprimir alfabeto en minuscula 
+ * 
+ *Return: Always 0
+ */
+int main(void)
+{
+     char c;
+
+	 
+
+
+
+
+	  (n/* your code goes there */
+  return (0);
+}
