905249207da3f314041c259e5b61eb28844ac2f5
Sandra <sandrajasminc@gmail.com>
2023-06-21 13:02:49 -0700

    if

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - imprimir numeros del 1 al 10  
+ * 
+ *Return: Always 0
+ */
+int main(void)
+{
+  int c = 'z';
+  while(c >= 'a')
+       {
+	 putchar(c);
+	 c--;
+       }
+   
+  putchar('\n');
+  return (0);
+
+  }
+
+
