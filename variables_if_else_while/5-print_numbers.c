45d73a454f04747857b28d3c4b02266dcfd8dad2
Sandra <sandrajasminc@gmail.com>
2023-06-23 15:27:11 -0700

    s

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,22 +3,19 @@
 #include <stdio.h>
 
 /**
- * main - imprimir numeros del 1 al 10  
- * 
+ ** main - imprimir numeros del 1 al 10
+ *
  *Return: Always 0
  */
 int main(void)
 {
-  int c = '0';
-  while(c <= '9')
-       {
-	 putchar(c);
-	 c++;
-       }
-   
-  putchar('\n');
-  return (0);
+	int c = '0';
 
-  }
-
-
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
