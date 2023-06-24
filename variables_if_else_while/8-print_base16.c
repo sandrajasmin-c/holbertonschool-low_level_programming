50051c8a1d3cd753ed597a0c5e0e22d3b505702d
Sandra <sandrajasminc@gmail.com>
2023-06-24 14:34:34 -0700

    bety

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,26 +3,25 @@
 #include <stdio.h>
 
 /**
- * main - imprimir numeros en base 16   
- * 
+ * main - imprimir numeros en base 16
+ *
  *Return: Always 0
  */
 int main(void)
 {
-  int c = '0';
-  while(c <= '9')
-       {
-	 putchar(c);
-	 c++;
-       }
-  c = 'a';
-  while(c <= 'f')
-      {
-       putchar(c);
-       c++;
-       }
-  putchar('\n');
-  return (0);
-  }
+	int c = '0';
 
-
+	while (c <= '9')
+	{
+	putchar(c);
+	c++;
+	}
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
