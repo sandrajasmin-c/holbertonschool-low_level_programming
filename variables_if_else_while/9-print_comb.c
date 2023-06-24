6ae02c409db51f01e54c0edc037a5f8d7bbdf739
Sandra <sandrajasminc@gmail.com>
2023-06-24 14:42:27 -0700

    s

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -3,25 +3,25 @@
 #include <stdio.h>
 
 /**
- * main - imprimir numeros en base 16   
- * 
+ * main - imprimir numeros en base 16
+ *
  *Return: Always 0
  */
 int main(void)
 {
-  int c = '0';
-  while(c <= '9')
-       {
-	 putchar(c);
-	 if (c != '9')
-	   {
-	     putchar(',');
-	     putchar(' ');
-	   }
-	 c++;
-       }
-  putchar('\n');
-  return (0);
- }
+	int c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ')
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
 
 
