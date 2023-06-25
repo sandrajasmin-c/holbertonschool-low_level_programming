261ea346c83093cdcc4ae5e221fab83ca4dbb918
Sandra <sandrajasminc@gmail.com>
2023-06-24 21:01:19 -0700

    s

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - imprimir alfabeto en minuscula
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	
+	if (letter != 'e' && letter != 'q')
+			putchar(C);
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
