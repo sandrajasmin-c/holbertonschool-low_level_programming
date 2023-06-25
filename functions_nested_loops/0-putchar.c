9ed89a5abb167f70518b08c6193b50278bc409cd
Sandra <sandrajasminc@gmail.com>
2023-06-25 14:56:51 -0700

    s

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+  
+ /**
+ * main - imprimir putchar 
+ * Return: Always (success)
+ */
+ 
+	int main(void) 
+{
+
+    		
+printf("_putchar\n");
+    return 0;
+
+}
+
+
+ 
