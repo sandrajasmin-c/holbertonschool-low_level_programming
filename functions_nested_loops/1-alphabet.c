7d9b08139f23f292e21e15344bc15d0578abdbdb
Sandra <sandrajasminc@gmail.com>
2023-06-26 09:45:10 -0700

    update

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+*print_alphabet- imprimir el alfabeto
+* Return: void
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main  print 2-print_alphabet_x10.c
+* Return: void 
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	
+	while (i <=0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+
+	_putchar ('\n');
+	i++;
+	}
+}
+		
