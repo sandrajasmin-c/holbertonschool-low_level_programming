3c42b8575d28d68a08f48a52f154856747dbc3ba
Sandra <sandrajasminc@gmail.com>
2023-06-27 14:12:17 -0700

    s

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,24 +1,18 @@
 #include "main.h"
- 
+
 /**
- * main - veririfcar caracter de minuscula.
- *
- * Return: Always 0 o 1
- */
+*_isalpha - Checks for alphabetic character
+*@c: The character to be checked
+*
+*Return:imprimir 1 para minuscula y o otros
+*/
 
-	int _isalpha(int c);
+	int _isalpha(int c)
+
 {
- 
-	int i = 0;
-
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-{	
-	return (1);
-}
-	
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
 	return (0);
 }
-
-    
-
-
--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * main - Write a function that prints the sign of a number.
- *
- * Return: Always 0. o 1
- */
+*@s1: main - Write a function that prints the sign of a number.
+*@s2:
+*@n: 
+*
+* Return: Always 0. o 1
+*/
 
 int print_sign(int n);
 
-int = n
-for ($n > 0 == 1; $i 0 = 0; $i <0 =-1)
-if ( n >= 0 & 1)
+int = n1, n2, n3
+if  (n1 > 0 && n2 ==0 && <0 =-1)
+{ 
+}
 	putchar 
 
 
