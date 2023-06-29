f7aee831aec6078ef8d509ecd77ce90e5d7ad8eb
Sandra <sandrajasminc@gmail.com>
2023-06-29 10:53:13 -0700

    reset

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 
 /**
-*main- updates the value it points to to 98
-*@n: puntero
-*Return: la direccion de la memoria o puntero
+*reset_to_98 - localizador de la memoria en el U
+* el valor del puntero es 98
+*
+*@n:lozalizar variable del puntero
+*Return : la direccion de la memoria o puntero
 *la función reset_to_98 es una función en C de 98
 */
 
