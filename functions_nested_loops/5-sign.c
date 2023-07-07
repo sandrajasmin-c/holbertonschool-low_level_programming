0bdf25380bc811a949ef983d8408fc5a8ab7e468
Sandra <sandrajasminc@gmail.com>
2023-07-07 12:21:42 -0700

    s

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*main - print_sign Prints the sign of a number.
+*print_sign Prints the sign of a number.
 *@n: imprime a caracter un numero
 *
 * Return: 1 and n = 0 y 0 si n = 0 and -1 si n es negativo
