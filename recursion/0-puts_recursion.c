7bdb4682fc959ecbe275fdfe35a50a27be14c929
Sandra <sandrajasminc@gmail.com>
2023-07-09 15:29:17 -0700

    s

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,15 +7,15 @@
  * Return:  1.
  */
 
-void _puts_recursion(char *s)
+	void _puts_recursion(char *s)
 {
-if (*s != '\0')
-{
-_putchar (*(s));
-_puts_recursion (s + 1);
-}
-else
-{
-putchar ('\n');
-}
+	if (*s != '\0')
+	{
+	_putchar (*(s));
+	_puts_recursion (s + 1);
+	}
+	else
+	{
+	putchar ('\n');
+	}
 }
